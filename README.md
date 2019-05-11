e12b1aa5802b94213abdee3eb71744873940ec32
Gary Bishop <gary.bishop.unc@gmail.com>
2019-05-11 14:20:30 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # prevent-stray-touchpad-clicks
-Prevent stray clicks on my XPS 13 touchpad by enabling tap-to-click only after a period of pad activity
+
+Prevent stray clicks on my XPS 13 touchpad by enabling tap-to-click only after
+a period of pad activity.
+
+The touchpad on my Dell XPS 13 Developer Edition drives me crazy by registering
+stray clicks when my hands come too near while typing. I've seen a few applications
+intended to help with this by disabling the touchpad during typing. They didn't
+do the job for me. I found that it doesn't only happen while typing is underway
+but often just as I start to type. 
+
+I reasoned that a better strategy would be to monitor the touchpad and only enable
+tap-to-click after a short period of activity. How often is the cursor exactly
+where you want it and all you need to do is swoop in and tap the pad? Never for me.
+The more common scenerio is I slide my finger on the pad to get the cursor to the 
+correct spot and then tap. 
+
+With this script the touchpad is always enabled and actually clicking always works;
+I'm only controlling the `tap-to-click` flag.
+
+I access the Linux /dev/input/eventN interface using the
+[evdev](https://python-evdev.readthedocs.io/en/latest/) module to monitor the
+touchpad and
+[gsettings](http://manpages.ubuntu.com/manpages/bionic/man1/gsettings.1.html)
+to control `tap-to-click`. I had to add myself to the `input` group to get
+access to the device.
+
+I start it from `Startup Applications`.
---
+++ touchpad.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+'''
+Enable tap-to-click only after a period of touchpad activity.
+My goal is the prevent stray clicks.
+
+Uses /dev/input/eventN to watch for touchpad activity and
+gsettings to enable and disable tap-to-click.
+
+I had to add myself the the 'input' group to get access
+to the device.
+
+Gary Bishop May 2019
+'''
+
+import evdev
+from select import select
+from subprocess import run
+import argparse
+
+parser = argparse.ArgumentParser(description='Manage tap-to-click on the touchpad')
+parser.add_argument('-v', action='store_true', help='show tap-to-click state')
+parser.add_argument('--device', help='path to touchpad device')
+parser.add_argument('--tap_delay', type=float, default=0.5,
+                    help='time to wait before enabling tap-to-click')
+parser.add_argument('--max_wait_idle', type=float, default=0.25,
+                    help='max idle time while waiting to enable tap')
+parser.add_argument('--max_tap_idle', type=float, default=1.0,
+                    help='max idle time before disabling tap again')
+args = parser.parse_args()
+
+verbose = args.v
+
+def tapToClick(on):
+    '''Control tap-to-click'''
+    if verbose:
+        print('tap', on)
+    run(['/usr/bin/gsettings',
+         'set',
+         'org.gnome.desktop.peripherals.touchpad',
+         'tap-to-click',
+         'true' if on else 'false'
+         ], check=True)
+
+
+# find the touchpad
+if not args.device:
+    for path in evdev.list_devices():
+        tp = evdev.InputDevice(path)
+        if 'touchpad' in tp.name.lower():
+            break
+    else:
+        raise RuntimeError('Touchpad not found')
+else:
+    tp = evdev.InputDevice(args.device)
+
+# define the states
+idle, wait, tap = 'idle wait tap'.split()
+
+# Configure the various times
+tap_delay = args.tap_delay  # time active before enabling tap to click
+# timeouts for the select while waiting for events
+timeout = {
+    idle: None,
+    wait: args.max_wait_idle,
+    tap: args.max_tap_idle,
+}
+
+# start in the idle state with tap off
+tapToClick(False)
+state = idle
+try:
+    while True:
+        # wait for an event or timeout
+        r, w, x, = select([tp.fd], [], [], timeout[state])
+        if not r:  # timeout
+            if state == tap:
+                tapToClick(False)
+            state = idle
+            continue
+
+        # get the event
+        event = tp.read_one()
+        # if idle enter the wait state and remember the time
+        if state == idle:
+            state = wait
+            t0 = event.timestamp()
+
+        # if in wait and its been long enough, enable tap
+        elif (state == wait and
+              event.timestamp() - t0 > args.tap_delay):
+            state = tap
+            tapToClick(True)
+finally:
+    # make sure tap is off when we leave
+    tapToClick(False)
